de9662cb3e727613d1c4f1201d08906a25cddc79
tking20 <60520848+tking20@users.noreply.github.com>
2020-02-24 13:05:26 -0600

    cs cat ascii

---
+++ Solutions/CSCatASCII.java
@@ -0,0 +1,24 @@
+
+public class CSCatASCII {
+	
+	public static void main(String [] args) {
+		
+		System.out.println("****.****.****.****.****.****.");
+		System.out.println("   _");
+		System.out.println("  ( \\");
+		System.out.println("   ) )");
+		System.out.println("  ( (  .-\"\"-.  A.-.A");
+		System.out.println("   \\ \\/      \\/ 0 1 \\");
+		System.out.println("    \\   \\    =;  t  /=");
+		System.out.println("     \\   |\"\".  ',--'");
+		System.out.println("      / //  | ||");
+		System.out.println("     /_,))  |_,))");
+		System.out.println("****.****.****.****.****.****.");
+
+		
+		
+		
+		
+	}
+
+}
